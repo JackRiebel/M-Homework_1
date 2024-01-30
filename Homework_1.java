7c3188092890dfb4f3589d39eefe44d6f53e83e9
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-01-30 14:07:52 -0600

    Add files via upload
    Testing out the upload since this is my first time using GitHub. Jack and my names were added to the file.

---
+++ Homework_1.java
@@ -0,0 +1,12 @@
+package Homework_1;
+
+public class Homework_1 
+{
+	
+	public static void main(String[] arg)
+	{
+		System.out.println("Name: Jack Riebel	ID: JackRiebel");
+		System.out.println("Name: Spencer Hunt	ID: sghd6");
+	}
+
+}
