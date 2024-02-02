1f0795f83261c503c78a61167188b1526ab1cce4
Jack Riebel <71671813+JackRiebel@users.noreply.github.com>
2024-02-01 18:59:31 -0600

    Update Homework_1.java

--- Homework_1.java
+++ Homework_1.java
@@ -5,10 +5,10 @@ public class Homework_1
 	
 	public static void main(String[] arg)
 	{
-		System.out.println("Name: Jack Riebel	ID: JackRiebel");
-		System.out.println("Name: Spencer Hunt	ID: sghd6");
+		System.out.println("Name: Jack Riebel		ID: JackRiebel");
+		System.out.println("Name: Spencer Hunt		ID: sghd6");
 		System.out.println("Name: Grant Eckhardt	ID: GrantE2003");
-		System.out.println("Name: Insert Here	ID: Insert Here");
+		System.out.println("Name: Insert Here		ID: Insert Here");
 	}
 
 }
